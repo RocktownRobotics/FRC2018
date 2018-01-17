0493c1b83d302549868cf9d033397bd11e7b54dc
RocktownRobotics <rocktownrobotics@gmail.com>
2018-01-16 23:38:02 -0500

    Fully Converted Most of DriveTrain code

--- FRC-2018/src/org/usfirst/frc/team3274/robot/subsystems/DriveTrain.java
+++ FRC-2018/src/org/usfirst/frc/team3274/robot/subsystems/DriveTrain.java
@@ -25,24 +25,31 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
  * information about it's speed and position.
  */
 public class DriveTrain extends Subsystem {
-	public static final double DEFAULT_SNIPER_MODE_MULTIPLIER = .5;
 
 	public static final int ENCODER_PULSES_PER_REVOLUTION = 256;
-	private double sniperMode = 1; // adds precision when less than to 1. Normal
-									// power is divided by this value to enter
-									// sniper mode
+
+	/**
+	 * Normal power is multiplied by this value when in enter sniper mode. Greater
+	 * than 0 and less than 1.
+	 */
+	private static final double SNIPER_MODE_MULTIPLIER = 0.5;
 
 	/** In inches **/
 	public static final double WHEEL_DIAMETER = 4.0;
 
-	// Four wheels
-	private TalonSRX _rightMotor = new TalonSRXGroup(RobotMap.REAR_RIGHT_MOTOR, RobotMap.FRONT_RIGHT_MOTOR, RobotMap.RIGHT_MOTOR);
-	private TalonSRX _leftMotor = new TalonSRXGroup(RobotMap.FRONT_LEFT_MOTOR, RobotMap.REAR_LEFT_MOTOR, RobotMap.LEFT_MOTOR);
-	
-	private Encoder rightEncoder = new Encoder(RobotMap.RIGHT_ENCODER[0], RobotMap.RIGHT_ENCODER[1], true,
-			EncodingType.k4X);
-	private Encoder leftEncoder = new Encoder(RobotMap.LEFT_ENCODER[0], RobotMap.LEFT_ENCODER[1], true,
-			EncodingType.k4X);
+	private boolean isSniperMode;
+
+	// "rightMotor" and "leftMotor" are
+	// actually all three motors on the side but should act as one
+	private TalonSRX _rightMotor = new TalonSRXGroup(RobotMap.REAR_RIGHT_MOTOR,
+			RobotMap.FRONT_RIGHT_MOTOR, RobotMap.RIGHT_MOTOR);
+	private TalonSRX _leftMotor = new TalonSRXGroup(RobotMap.FRONT_LEFT_MOTOR,
+			RobotMap.REAR_LEFT_MOTOR, RobotMap.LEFT_MOTOR);
+
+	private Encoder rightEncoder = new Encoder(RobotMap.RIGHT_ENCODER[0], RobotMap.RIGHT_ENCODER[1],
+			true, EncodingType.k4X);
+	private Encoder leftEncoder = new Encoder(RobotMap.LEFT_ENCODER[0], RobotMap.LEFT_ENCODER[1],
+			true, EncodingType.k4X);
 
 	public DriveTrain() {
 
@@ -50,25 +57,14 @@ public class DriveTrain extends Subsystem {
 		rightEncoder.setPIDSourceType(PIDSourceType.kDisplacement);
 		leftEncoder.setPIDSourceType(PIDSourceType.kDisplacement);
 
-		if (Robot.isReal()) { // Converts to feet
+		isSniperMode = true;
 
-			double distancePerPulse; // in feet
-			// distancePerPulse = (WHEEL_DIAMETER/* in */ * Math.PI)
-			// / (ENCODER_PULSES_PER_REVOLUTION * 12.0/* in/ft */);
-			// distancePerPulse = 4922.041667 / (ENCODER_PULSES_PER_REVOLUTION
-			// * ENCODER_PULSES_PER_REVOLUTION); // in feet
+		double distancePerPulse; // in feet
+		distancePerPulse = (WHEEL_DIAMETER/* in */ * Math.PI)
+				/ (ENCODER_PULSES_PER_REVOLUTION * 12.0/* in/ft */);
 
-			distancePerPulse = 0.00078594; // in feet
-
-			rightEncoder.setDistancePerPulse(distancePerPulse);
-			leftEncoder.setDistancePerPulse(distancePerPulse);
-		} else { // Convert to feet 4in diameter wheels with 256 tick simulated
-					// encoders
-			rightEncoder.setDistancePerPulse(
-					(WHEEL_DIAMETER/* in */ * Math.PI) / (ENCODER_PULSES_PER_REVOLUTION * 12.0/* in/ft */));
-			leftEncoder.setDistancePerPulse(
-					(WHEEL_DIAMETER/* in */ * Math.PI) / (ENCODER_PULSES_PER_REVOLUTION * 12.0/* in/ft */));
-		}
+		rightEncoder.setDistancePerPulse(distancePerPulse);
+		leftEncoder.setDistancePerPulse(distancePerPulse);
 	}
 
 	/**
@@ -80,6 +76,101 @@ public class DriveTrain extends Subsystem {
 		setDefaultCommand(new DriveWithJoystick());
 	}
 
+	/**
+	 * Limit motor values to the -1.0 to +1.0 range.
+	 */
+	protected double limit(double value) {
+		if (value > 1.0) {
+			return 0.999;
+		}
+		if (value < -1.0) {
+			return -0.999;
+		}
+		return value;
+	}
+
+	/**
+	 * Returns 0.0 if the given value is within the specified range around zero. The
+	 * remaining range between the deadzone and 1.0 is scaled from 0.0 to 1.0.
+	 * 
+	 * This method is used to make sure we ignore false input from the joysticks
+	 * (basically any value that is too small).
+	 *
+	 * @param value
+	 *            value to clip
+	 * @param deadband
+	 *            range around zero
+	 */
+	protected double applyDeadband(double value, double deadband) {
+		if (Math.abs(value) > deadband) {
+			if (value > 0.0) {
+				return (value - deadband) / (1.0 - deadband);
+			} else {
+				return (value + deadband) / (1.0 - deadband);
+			}
+		} else {
+			return 0.0;
+		}
+	}
+
+	/**
+	 * Drive the wheels on one side forward with one stick and the wheels on another
+	 * side forward with another stick.
+	 * 
+	 * @param joy
+	 *            PS3 style joystick to use as the input for tank drive.
+	 */
+	public void tankDrive(Joystick joy) {
+		this.tankDrive(joy.getRawAxis(-RobotMap.XBOX_LEFT_Y_AXIS),
+				joy.getRawAxis(-RobotMap.XBOX_RIGHT_X_AXIS));
+	}
+
+	/**
+	 * Drive the wheels on one side forward with one stick and the wheels on another
+	 * side forward with another stick.
+	 * 
+	 * @param leftAxis
+	 *            left stick y-axis (or power between -1 and 1)
+	 * @param rightAxis
+	 *            right stick y-axis (or power between -1 and 1)
+	 */
+	public void tankDrive(double leftAxis, double rightAxis) {
+		tankDrive(-leftAxis, -rightAxis, true);
+	}
+
+	/**
+	 * Drive the wheels on one side forward with one stick and the wheels on another
+	 * side forward with another stick.
+	 * 
+	 * @param leftPower
+	 *            power towards left motor, between -1 and 1, where 0 is not moving
+	 * @param rightPower
+	 *            power towards right motor, between -1 and 1, where 0 is not moving
+	 * @param applyDeadband
+	 *            whether or not tankdrive should ignore smaller inputs, in order to
+	 *            account for false input from joysticks.
+	 */
+	public void tankDrive(double leftPower, double rightPower, boolean applyDeadband) {
+		double lJoyStickVal = 0.0;
+		double rJoyStickVal = 0.0;
+
+		if (applyDeadband) {
+			lJoyStickVal = applyDeadband(leftPower, OI.JOYSTICK_DEADZONE);
+			rJoyStickVal = applyDeadband(rightPower, OI.JOYSTICK_DEADZONE);
+		}
+
+		// used for fine movements
+		if (isSniperMode) {
+			lJoyStickVal *= SNIPER_MODE_MULTIPLIER;
+			rJoyStickVal *= SNIPER_MODE_MULTIPLIER;
+		}
+
+		_leftMotor.set(ControlMode.PercentOutput, lJoyStickVal);
+		_rightMotor.set(ControlMode.PercentOutput, rJoyStickVal);
+
+		Timer.delay(0.005); // wait for a motor update time
+	}
+
 	public int getLeftRotations() {
 		return this.leftEncoder.getRaw();
 	}
@@ -89,21 +180,29 @@ public class DriveTrain extends Subsystem {
 	}
 
 	/**
+	 * Used to enable or disable sniper mode. Sniper mode makes the robot more
+	 * sensitive to actions from the driver.
+	 * 
+	 * @param isEnabled
+	 *            whether or not sniper mode is enabled
+	 */
+	public void setSniperMode(boolean isEnabled) {
+		this.isSniperMode = isEnabled;
+	}
+
+	/**
+	 * Stop the drivetrain from moving.
+	 */
+	public void stop() {
+		this.tankDrive(0, 0);
+	}
+
+	/**
 	 * Resets encoders to start tracking distance driven from a certain point.
 	 */
 	public void resetEncoders() {
 		rightEncoder.reset();
 		leftEncoder.reset();
-	}
-
-	/**
-	 * 
-	 * @param dividor
-	 *            - 1 is not sniper mode, anything between 0 and 1 is the percentage
-	 *            the motors are reduced by
-	 */
-	public void setSniperMode(double dividor) {
-		this.sniperMode = dividor;
 	}
 
 	/**
@@ -116,7 +215,6 @@ public class DriveTrain extends Subsystem {
 		double dist;
 
 		dist = (rightEncoder.getDistance() + leftEncoder.getDistance()) / 2;
-		// dist = rightEncoder.getDistance();
 
 		return dist;
 	}
@@ -139,224 +237,6 @@ public class DriveTrain extends Subsystem {
 		return rightEncoder.getDistance();
 	}
 
-	// Checks to see if a number (variable) is in a certain range (rangeFrom)
-	// from a certain point (point)
-	public boolean inRange(double variable, double point, double rangeFrom) {
-		boolean result = false;
-		if (variable < (point + rangeFrom) && variable > (point - rangeFrom)) {
-			result = true;
-		}
-		return result;
-	}
-
-	/**
-	 * For joystick axis. Checks if the given axis is OI.DEAD_ZONE distance from 0.
-	 * 
-	 * @param axis
-	 *            - joystick axis
-	 * @return true if axis value is in dead zone
-	 */
-	public boolean isInDeadZone(double axis) {
-		if ((axis < OI.DEAD_ZONE) && (axis > -OI.DEAD_ZONE)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	/**
-	 * Drive the wheels on one side forward with one stick and the wheels on another
-	 * side forward with another stick.
-	 * 
-	 * @param joy
-	 *            PS3 style joystick to use as the input for tank drive.
-	 */
-	public void tankDrive(Joystick joy) {
-		this.tankDrive(joy.getRawAxis(-RobotMap.XBOX_LEFT_Y_AXIS), joy.getRawAxis(-RobotMap.XBOX_RIGHT_X_AXIS));
-	}
-
-	/**
-	 * Drive the wheels on one side forward with one stick and the wheels on another
-	 * side forward with another stick.
-	 * 
-	 * @param leftAxis
-	 *            left stick y-axis
-	 * @param rightAxis
-	 *            right stick y-axis
-	 */
-	public void tankDrive(double leftAxis, double rightAxis) {
-		double lJoyStickVal = 0.0;
-		double rJoyStickVal = 0.0;
-
-		// Setting up Deadzones
-		if (this.isInDeadZone(leftAxis)) {
-			lJoyStickVal = 0.0;
-		} else {
-			lJoyStickVal = leftAxis;
-		}
-
-		if (this.isInDeadZone(rightAxis)) {
-			rJoyStickVal = 0.0;
-		} else {
-			rJoyStickVal = rightAxis;
-		}
-
-		// used for sniper mode
-		lJoyStickVal *= sniperMode;
-		rJoyStickVal *= sniperMode;
-
-		double[] correctedPow = getSpeedCorrection(-lJoyStickVal, -rJoyStickVal);
-
-	    _leftMotor.set(ControlMode.PercentOutput, correctedPow[0]);
-	    _rightMotor.set(ControlMode.PercentOutput, correctedPow[1]);
-		
-		Timer.delay(0.005); // wait for a motor update time
-
-	}
-
-	/**
-	 * Use y-axis on left stick for power and x-axis on right stick for turning.
-	 * 
-	 * @param joy
-	 *            joystick
-	 */
-	public void carDrive(Joystick joy) {
-		this.carDrive(joy.getRawAxis(RobotMap.XBOX_LEFT_Y_AXIS), joy.getRawAxis(RobotMap.XBOX_RIGHT_X_AXIS));
-	}
-
-	/**
-	 * Use y-axis on left stick for power and x-axis on right stick for turning.
-	 * 
-	 * @param leftAxis
-	 *            y-axis of left stick
-	 * @param rightAxis
-	 *            x-axis of right stick
-	 */
-	public void carDrive(double leftAxis, double rightAxis) {
-		boolean inLDeadZone = false;
-
-		double lJoyStickVal = 0.0;
-		double rJoyStickVal = 0.0;
-
-		double leftPower;
-		double rightPower;
-
-		// Setting up Deadzones
-		if (this.isInDeadZone(leftAxis)) {
-			lJoyStickVal = 0.0;
-			inLDeadZone = true;
-		} else {
-			lJoyStickVal = -leftAxis;
-		}
-
-		if (this.isInDeadZone(rightAxis)) {
-			rJoyStickVal = 0.0;
-		} else {
-			rJoyStickVal = rightAxis;
-		}
-
-		// used for sniper mode
-		lJoyStickVal *= sniperMode;
-		rJoyStickVal *= sniperMode;
-
-		// convert to left and right power with floating point errors in mind
-		leftPower = lJoyStickVal;
-		rightPower = lJoyStickVal;
-
-		if (!inLDeadZone) {
-
-			// turns with throttle
-
-			if (rJoyStickVal < -0.01) {
-				leftPower = leftPower * (1 - Math.abs(rJoyStickVal));
-			} else if (rJoyStickVal > 0.01) {
-				rightPower = rightPower * (1 - Math.abs(rJoyStickVal));
-			}
-		} else {
-			// does a point turn if there is no throttle
-			double pow = Math.abs(rJoyStickVal);
-
-			if (rJoyStickVal < -0.01) {
-				leftPower = -pow;
-				rightPower = pow;
-			} else if (rJoyStickVal > 0.01) {
-				leftPower = pow;
-				rightPower = -pow;
-			}
-		}
-
-		// double[] correctedPow = { -leftPower, -rightPower };
-		double[] correctedPow = getSpeedCorrection(-leftPower, -rightPower);
-
-		drive.tankDrive(correctedPow[0], correctedPow[1]);
-		Timer.delay(0.005); // wait for a motor update time
-	}
-
-	/**
-	 * Stop the drivetrain from moving.
-	 */
-	public void stop() {
-		drive.tankDrive(0, 0);
-	}
-
-	/**
-	 * Calculates a correction for the speed the motors should be moving at based on
-	 * difference in encoder readings.
-	 * 
-	 * Makes sure that the robot is moving in the direction it should be moving in,
-	 * AKA perfectly straight if it should be going straight.
-	 * 
-	 * @param leftPower
-	 *            - un-corrected power for left wheels (between -1 and 1)
-	 * @param rightPower
-	 *            - un-corrected power for right wheels (between -1 and 1)
-	 * 
-	 * @return correction for motor power in the form { leftPower, rightPower }
-	 */
-	public double[] getSpeedCorrection(double leftPower, double rightPower) {
-
-		final double ALLOWED_MARGIN_OF_ERROR = .005; // as a percentage between
-														// 0
-														// and 1 (0.005 is good)
-
-		double[] corrected = { leftPower, rightPower };
-
-		double rightEncoderRate = rightEncoder.getRate(); // feet per second
-		double leftEncoderRate = leftEncoder.getRate(); // feet per second
-
-		double leftToRightMotor; // ratio between the power in the left and
-									// right wheels
-		double leftToRightEncoder; // ratio between the rate in the left and
-									// right encoders
-
-		// add code for correcting power here
-		leftToRightMotor = leftPower / rightPower;
-		leftToRightEncoder = leftEncoderRate / rightEncoderRate;
-
-		double diff = leftToRightEncoder - leftToRightMotor;
-
-		if (diff > -ALLOWED_MARGIN_OF_ERROR && diff < ALLOWED_MARGIN_OF_ERROR) {
-			if (Math.abs(leftToRightMotor) > 1) {
-				corrected[1] = leftPower / leftToRightEncoder;
-				corrected[1] = Math.copySign(corrected[1], rightPower);
-			} else if (Math.abs(leftToRightMotor) < 1) {
-				corrected[0] = rightPower * leftToRightEncoder;
-				corrected[0] = Math.copySign(corrected[0], leftPower);
-			}
-		}
-
-		// Make sure neither value is above 1 or below -1
-		for (int i = 0; i < 2; i++) {
-			if (corrected[i] > 1) {
-				corrected[i] = .999;
-			} else if (corrected[i] < -1) {
-				corrected[i] = -.999;
-			}
-		}
-		System.out.println(String.format("LeftEncoder Value = %f ", leftEncoder.getRate()));
-		return corrected;
-	}
-
 	/**
 	 * @return The encoder getting the distance and speed of left side of the
 	 *         drivetrain.
@@ -373,9 +253,6 @@ public class DriveTrain extends Subsystem {
 		return rightEncoder;
 	}
 
-	public double getSniperValue() {
-		return this.sniperMode;
-	}
 	//
 	// /**
 	// * @return The current angle of the drivetrain.
--- FRC-2018/src/org/usfirst/frc/team3274/robot/util/TalonSRXGroup.java
+++ FRC-2018/src/org/usfirst/frc/team3274/robot/util/TalonSRXGroup.java
@@ -9,24 +9,24 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
  * @author AJ Snarr
  */
 public class TalonSRXGroup extends TalonSRX {
-	
+
 	private TalonSRX[] motors;
-	
+
 	/**
 	 * Creates a group with the given motors.
 	 * 
-	 * @param motors 
+	 * @param motors
 	 */
 	public TalonSRXGroup(Integer... portNums) {
 		// initialize the first motor given
 		super(portNums[0]);
-		
+
 		// initialize all other motors given
 		this.motors = new TalonSRX[portNums.length - 1];
 		for (int i = 1; i < portNums.length; i++) {
 			motors[i - 1] = new TalonSRX(portNums[i]);
 		}
-		
+
 		// make every motor become a slave to the first motor
 		for (TalonSRX mot : this.motors) {
 			mot.follow(this);
