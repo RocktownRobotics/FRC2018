8072e9fb384a71a6e217e1b12ea6a93fa4f6f1e6
RocktownRobotics <rocktownrobotics@gmail.com>
2018-02-11 17:28:16 -0500

    Fixed String Checking

--- FRC-2018/src/org/usfirst/frc/team3274/robot/commands/autonomous/groups/Primary_Autonomous.java
+++ FRC-2018/src/org/usfirst/frc/team3274/robot/commands/autonomous/groups/Primary_Autonomous.java
@@ -13,37 +13,36 @@ public class Primary_Autonomous extends CommandGroup {
 	private String startPosition;
 	private String scoringStrategy;
 	private double initialDelay;
-	
-	public boolean switchIsRight() {
-		if (Robot.gameData == "RLR") {
-			return true;
-		}
-		else {
-			if(Robot.gameData == "RRR") {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-	}
-	
-	public boolean scaleIsRight() {
-		if (Robot.gameData == "RRR") {
-			return true;
-		}
-		else {
-			if(Robot.gameData == "LRL") {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-	}
-	
-	
 
+	public boolean switchIsRight() {
+		// check if string has 3 characters
+		if (Robot.gameData.length() >= 3) {
+			// check if first character in string is 'R'
+			if (Robot.gameData.charAt(0) == 'R') {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			DriverStation.reportError("Failed to read a correct FMS message", true);
+			return false;
+		}
+	}
+
+	public boolean scaleIsRight() {
+		// check if string has 3 characters
+		if (Robot.gameData.length() >= 3) {
+			// check if second character in string is 'R'
+			if (Robot.gameData.charAt(1) == 'R') {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			DriverStation.reportError("Failed to read a correct FMS message", true);
+			return false;
+		}
+	}
 
 	/**
 	 * Here is how you would make the robot drive forward 3 feet and then turn left
@@ -54,12 +53,13 @@ public class Primary_Autonomous extends CommandGroup {
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	/**
 	 * 
-	 * All of the if(BLAH == "BLEH") needs to become if(BLAH .equals "BLEH")
+	 * All of the if(BLAH.equals("BLEH") needs to become if(BLAH .equals "BLEH")
 	 * 
 	 * This is a very complicated bit of code, with many, many stacked if
 	 * statements, not written for easy readability. You have been warned...
 	 * 
-	 * Additionally, if you are using my EditBox settings, your eyes will be bleeding by the end of it all...
+	 * Additionally, if you are using my EditBox settings, your eyes will be
+	 * bleeding by the end of it all...
 	 */
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -71,124 +71,105 @@ public class Primary_Autonomous extends CommandGroup {
 		this.initialDelay = startDelay;
 		this.scoringStrategy = scoreSelection;
 		this.startPosition = startPos;
-		
-		
 
 		addSequential(new Delay(this.initialDelay));
 
-		if (this.scoringStrategy == "Switch") {
+		if (this.scoringStrategy.equals("Switch")) {
 
-			if (this.startPosition == "Left") {
-				if(this.switchIsRight()) {
-					//left to right switch
+			if (this.startPosition.equals("Left")) {
+				if (this.switchIsRight()) {
+					// left to right switch
 					addSequential(new LeftToRight());
 					addSequential(new RightStartToSwitch());
-				}
-				else {
-					
+				} else {
+
 					// going from left to left switch
 					addSequential(new LeftToLeft());
 					addSequential(new LeftStartToSwitch());
 				}
-			} else if (this.startPosition == "Middle") {
-				
-				if(this.switchIsRight()) {
-					//mid to Right Switch
+			} else if (this.startPosition.equals("Middle")) {
+
+				if (this.switchIsRight()) {
+					// mid to Right Switch
 					addSequential(new MidToRight());
 					addSequential(new RightStartToSwitch());
-				}else {
-					//Mid to Left Switch
+				} else {
+					// Mid to Left Switch
 					addSequential(new MidToLeft());
 					addSequential(new LeftStartToSwitch());
 				}
+			} else {
+				if (this.switchIsRight()) {
+					// left to right switch
+					addSequential(new RightToRight());
+					addSequential(new RightStartToSwitch());
 				} else {
-					if(this.switchIsRight()) {
-						//left to right switch
-						addSequential(new RightToRight());
-						addSequential(new RightStartToSwitch());
-					}
-					else {
-						//right to Left Switch
-						addSequential(new RightToLeft());
-						addSequential(new LeftStartToSwitch());
-					}
+					// right to Left Switch
+					addSequential(new RightToLeft());
+					addSequential(new LeftStartToSwitch());
 				}
 			}
-
-		
-
-		else if (this.scoringStrategy == "Scale") {
-
-				if (this.startPosition == "Left") {
-					if(this.scaleIsRight()) {
-						//left to right scale
-						addSequential(new LeftToRight());
-						addSequential(new RightStartToScale());
-					}
-					else {
-						//left to left scale
-						addSequential(new LeftToLeft());
-						addSequential(new LeftStartToScale());
-					}
-					
-					
-				} else if (this.startPosition == "Middle") {
-					if(this.scaleIsRight()) {
-						//mid to right scale
-						addSequential(new MidToRight());
-						addSequential(new RightStartToScale());
-					}
-					else {
-						//mid to left scale
-						addSequential(new MidToLeft());
-						addSequential(new LeftStartToScale());
-					}
-					} else {
-						if(this.scaleIsRight()) {
-							//right to right scale
-							addSequential(new RightToRight());
-							addSequential(new RightStartToSwitch());
-						}
-						else {
-							//right to left scale
-							addSequential(new RightToLeft());
-							addSequential(new LeftStartToSwitch());
-						}
-					}
 		}
-				
 
-			
+		else if (this.scoringStrategy.equals("Scale")) {
 
-			else if (this.scoringStrategy == "Exchange") {
-
-					if (this.startPosition == "Left") {
-						System.out.println("Failed: Cannot move to exchange from Left Start Position. However, Robot is smart and will still cross the auto line");
-						addSequential(new LeftToLeft());
-					} else if (this.startPosition == "Middle") {
-						//mid to exchange to auto line
-						addSequential(new MidToExchange());
-						} else {
-							System.out.println("Failed: Cannot move to exchange from Right Start. Robot is sad, but will still cross the auto line");
-							addSequential(new RightToRight());
-						}
-					
-					
-					
-
-				}
-			//Cross the Auto Line.
-				else if (this.startPosition == "Left") {
+			if (this.startPosition.equals("Left")) {
+				if (this.scaleIsRight()) {
+					// left to right scale
+					addSequential(new LeftToRight());
+					addSequential(new RightStartToScale());
+				} else {
+					// left to left scale
 					addSequential(new LeftToLeft());
-					} else{
-						addSequential(new RightToRight());
-						} 
-					
+					addSequential(new LeftStartToScale());
+				}
 
-				
+			} else if (this.startPosition.equals("Middle")) {
+				if (this.scaleIsRight()) {
+					// mid to right scale
+					addSequential(new MidToRight());
+					addSequential(new RightStartToScale());
+				} else {
+					// mid to left scale
+					addSequential(new MidToLeft());
+					addSequential(new LeftStartToScale());
+				}
+			} else {
+				if (this.scaleIsRight()) {
+					// right to right scale
+					addSequential(new RightToRight());
+					addSequential(new RightStartToSwitch());
+				} else {
+					// right to left scale
+					addSequential(new RightToLeft());
+					addSequential(new LeftStartToSwitch());
+				}
+			}
+		}
+
+		else if (this.scoringStrategy.equals("Exchange")) {
+
+			if (this.startPosition.equals("Left")) {
+				System.out.println(
+						"Failed: Cannot move to exchange from Left Start Position. However, Robot is smart and will still cross the auto line");
+				addSequential(new LeftToLeft());
+			} else if (this.startPosition.equals("Middle")) {
+				// mid to exchange to auto line
+				addSequential(new MidToExchange());
+			} else {
+				System.out.println(
+						"Failed: Cannot move to exchange from Right Start. Robot is sad, but will still cross the auto line");
+				addSequential(new RightToRight());
 			}
 
 		}
+		// Cross the Auto Line.
+		else if (this.startPosition.equals("Left")) {
+			addSequential(new LeftToLeft());
+		} else {
+			addSequential(new RightToRight());
+		}
 
-	
+	}
 
+}
