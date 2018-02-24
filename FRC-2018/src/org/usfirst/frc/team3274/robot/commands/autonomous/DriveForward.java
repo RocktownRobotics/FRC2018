baea94a8b2459046592b1d1875ac9fb87a87bbb5
RocktownRobotics <rocktownrobotics@gmail.com>
2018-02-24 11:09:42 -0500

    Made driveforward accelerate more

--- FRC-2018/src/org/usfirst/frc/team3274/robot/commands/autonomous/DriveForward.java
+++ FRC-2018/src/org/usfirst/frc/team3274/robot/commands/autonomous/DriveForward.java
@@ -7,8 +7,10 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * @Author Ian McGary A command. That drives. Forward. Hence the name
- *         DriveForward.
+ * A command. That drives. Forward. Hence the name DriveForward.
+ * 
+ * @author Ian McGary
+ * @author AJ Snarr
  */
 
 public class DriveForward extends Command {
@@ -18,9 +20,12 @@ public class DriveForward extends Command {
 	public static final double SLOW_SPEED = 0.1;
 
 	public static final double SLOW_DISTANCE = 1; // offset in ft
-	private static final double MID_DISTANCE = 2;
+	public static final double MID_DISTANCE = 2; // offset in ft
+
+	public static final double SLOWER_STARTING_TIME = 2; // in seconds
 
 	private double targetDistance;
+	private double startTime;
 
 	/**
 	 * creates a new DriveForward command that drives the targetDistance.
@@ -39,12 +44,12 @@ public class DriveForward extends Command {
 		Robot.kDriveTrain.resetEncoders();
 		DriverStation.reportWarning("initDriveForward", false);
 		System.out.println("Robot is now driving forwards");
+
+		this.startTime = Robot.getTime();
 	}
 
 	@Override
 	protected void execute() {
-
-		System.out.println("exe_driveforward");
 
 		double determinedSpeed;
 		if (Robot.kDriveTrain.isRobotTipping()) {
@@ -53,7 +58,12 @@ public class DriveForward extends Command {
 					"Robot is almost tipping over. Robot is scared. Robot will move slowly and carefully until it is on safer ground...");
 		} else {
 			if (Robot.kDriveTrain.getDistanceDriven() < targetDistance - MID_DISTANCE) {
-				determinedSpeed = FAST_SPEED;
+
+				if (this.startTime + SLOWER_STARTING_TIME <= Robot.getTime()) {
+					determinedSpeed = FAST_SPEED;
+				} else {
+					determinedSpeed = NORMAL_SPEED;
+				}
 
 			} else if (Robot.kDriveTrain.getDistanceDriven() < targetDistance - SLOW_DISTANCE) {
 				determinedSpeed = SLOW_SPEED;
@@ -76,7 +86,6 @@ public class DriveForward extends Command {
 	protected void end() {
 		Robot.kDriveTrain.tankDrive(0, 0, false);
 		Robot.kDriveTrain.resetYaw();
-		DriverStation.reportWarning("endDriveForward", false);
 		System.out.println("No longer driving forwards");
 	}
 
