98f4bedac71011d9f9d3513e4a249161a6463e19
UnimpressiveGoo <UnimpressiveGoo@gmail.com>
2018-03-01 10:39:41 -0500

    Added new Auto programs

---
+++ FRC-2018/src/org/usfirst/frc/team3274/robot/commands/autonomous/programs/SwitchFromMid.java
@@ -0,0 +1,57 @@
+package org.usfirst.frc.team3274.robot.commands.autonomous.programs;
+
+import org.usfirst.frc.team3274.robot.Robot;
+import org.usfirst.frc.team3274.robot.commands.autonomous.ShiftDownForTime;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class SwitchFromMid extends CommandGroup {
+
+	/**
+	 * Here is how you would make the robot drive forward 3 feet and then turn left
+	 * 90 degrees.
+	 */
+	
+	public boolean switchIsRight() {
+		// check if string has 3 characters
+		if (Robot.gameData.length() >= 3) {
+			// check if first character in string is 'R'
+			if (Robot.gameData.toUpperCase().charAt(0) == 'R') {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			DriverStation.reportError("Failed to read a correct FMS message: " + Robot.gameData, true);
+			System.out.println(
+					"Failed: Field said something unintelligable... Robot hasn't a clue what it should do... Left is always the answer");
+			return false;
+		}
+	}
+
+	public boolean scaleIsRight() {
+		// check if string has 3 characters
+		if (Robot.gameData.length() >= 3) {
+			// check if second character in string is 'R'
+			if (Robot.gameData.toUpperCase().charAt(1) == 'R') {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			DriverStation.reportError("Failed to read a correct FMS message", true);
+			System.out.println(
+					"Failed: Field said something unintelligable... Robot hasn't a clue what it should do... Left is always the answer");
+
+			return false;
+		}
+	}
+	public SwitchFromMid() {
+		// make sure robot is in low gear
+		addSequential(new ShiftDownForTime());
+
+		// addSequential(new DriveForward(3));
+		// addSequential(new TurnRobot(-90));
+	}
+}
